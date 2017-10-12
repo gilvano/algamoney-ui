6729a7b4506c61916d75c287495b2c45cc20a1d6
gilpi <g.piontkoski@gmail.com>
2017-10-12 15:53:21 -0300

    Aula 17.13 - Implementando a exclusão de pessoas

--- src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
+++ src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { ErrorHandlerService } from './../../core/error-handler.service';
+import { ToastyService } from 'ng2-toasty';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
-import { LazyLoadEvent } from 'primeng/components/common/api';
+import { LazyLoadEvent, ConfirmationService } from 'primeng/components/common/api';
 
 import { PessoaFiltro, PessoaService } from './../pessoa.service';
 
@@ -14,8 +16,14 @@ export class PessoasPesquisaComponent implements OnInit {
   totalRegistros = 0;
   filtro = new PessoaFiltro();
   pessoas = [];
+  @ViewChild('tabela') grid;
 
-  constructor(private pessoaService: PessoaService) { }
+  constructor(
+    private pessoaService: PessoaService,
+    private toasty: ToastyService,
+    private confirmation: ConfirmationService,
+    private errorHandler: ErrorHandlerService
+  ) { }
 
   ngOnInit() {
   }
@@ -33,6 +41,31 @@ export class PessoasPesquisaComponent implements OnInit {
   aoMudarPagina(event: LazyLoadEvent) {
     const pagina = event.first / event.rows;
     this.pesquisar(pagina);
+  }
+
+  excluir(pessoa: any) {
+    this.pessoaService.excluir(pessoa.codigo)
+    .then(() => {
+      if (this.grid.first === 0 ) {
+        this.pesquisar();
+      } else {
+        this.grid.first = 0;
+      }
+
+      this.toasty.success('Pessoa excluída com sucesso!');
+    })
+    .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  confirmarExclusao(pessoa: any) {
+    this.confirmation.confirm({
+      header: 'Confirmação',
+      message: 'Tem certeza que deseja excluir?',
+      icon: 'fa fa-question-circle',
+      accept: () => {
+        this.excluir(pessoa);
+      }
+    });
   }
 
 }
